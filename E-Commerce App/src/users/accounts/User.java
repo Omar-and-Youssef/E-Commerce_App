adcb767483d36ee6f4909578d06ef624fb2f78cb
Youssef Abdelrahman <162054447+Enovac@users.noreply.github.com>
2024-12-03 16:58:27 +0100

    Created Super Class

--- E-Commerce App/src/users/accounts/User.java
+++ E-Commerce App/src/users/accounts/User.java
@@ -1,5 +1,53 @@
 package users.accounts;
+public abstract class User {
+    private String email,password;
+    protected String name,address,phoneNumber;
+    protected char gender;
+//=======================================Constructor===================================
+    public User(String name,String email,String password,String phoneNumber,
+           String address,char gender){
 
-public class User {
+    }
+    //Constructor for name email password gender
+    //Constructor for name email password gender address
+    //Constructor for name email password gender phoneNumber
+//=======================================Methods=======================================
+    public void changePassword(String newPassword){
+        //Put Conditions + salting+ hashing 
+    }
+//=======================================Get&Set=======================================
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public char getGender() {
+        return gender;
+    }
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
 
 }
