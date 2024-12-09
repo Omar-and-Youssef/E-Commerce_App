c56ff67dd31e1c7635eb6d700fb19e0348fd1ba6
Youssef Abdelrahman <162054447+Enovac@users.noreply.github.com>
2024-12-09 16:28:37 +0100

    Done

---
+++ E-Commerce App/src/controller/AppLauncher.java
@@ -0,0 +1,12 @@
+package controller;
+
+public class AppLauncher{
+    private static Engine app;
+    public static void main(String[]args){
+        new AppLauncher().launch();
+    }
+    void launch(){
+        app=new Engine();
+        //
+    }
+}
---
+++ E-Commerce App/src/controller/Screen.java
@@ -0,0 +1,5 @@
+package controller;
+
+public enum Screen {
+    LOGIN,SIGN_UP,HOME,PRODUCT,CART,ORDERS,WISH_LSIT;
+}
